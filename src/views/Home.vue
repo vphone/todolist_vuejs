bb488695b2a492d15616c0aa8d2316f79c8db1f2
vphone <veronique.phone@gmail.com>
2021-03-24 16:20:38 +0100

    vue 3 app

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Title msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Title from '@/components/Title.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Title
+  }
+}
+</script>
